3136648eebb6e830a72d6fd64fc2d1f05297655f
최미래 <miraechoi@dgsw.hs.kr>
2024-06-22 20:49:25 +0900

    feat :: user details services

---
+++ src/main/java/com/appjam/dopamine/domain/user/security/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.appjam.dopamine.domain.user.security.service;
+
+import com.appjam.dopamine.domain.user.entity.User;
+import com.appjam.dopamine.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 찾을 수 없습니다: " + email));
+        return UserDetailsImpl.build(userEntity);
+    }
+}
