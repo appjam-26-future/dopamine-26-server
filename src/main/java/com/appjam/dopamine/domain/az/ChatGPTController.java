46a28bfb6e7c41042e326a98ef9afd477cd99269
최미래 <miraechoi@dgsw.hs.kr>
2024-06-23 03:28:40 +0900

    feat :: create /chatqgt/gen api & swagger desc

--- src/main/java/com/appjam/dopamine/domain/az/ChatGPTController.java
+++ src/main/java/com/appjam/dopamine/domain/az/ChatGPTController.java
@@ -1,6 +1,8 @@
 package com.appjam.dopamine.domain.az;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,12 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/chatgpt")
+@Tag(name = "ai", description = "ai 관련 api 입니다.")
 public class ChatGPTController {
 
     private final ChatGPTService chatGPTService;
 
-    @PostMapping
+    @PostMapping("/image")
+    @Operation(summary = "이미지 판별 ai", description = "사진 안에 사람이 2명 이하면 0 아니면 1")
     public Object createData(@RequestBody MessageRequest imgUrl) throws JsonProcessingException {
         return chatGPTService.createData(imgUrl.message());
+    }
+
+    @PostMapping("/gen")
+    @Operation(summary = "ai 도파민 미션 루틴", description = "일주일간의 미션을 ai로 계획합니다.")
+    public Object genData(@RequestBody Gen gen) throws JsonProcessingException {
+        return chatGPTService.genData(gen);
     }
 }
