50f5acda162427bc58c0da6025dcc71bbb76df0d
최미래 <miraechoi@dgsw.hs.kr>
2024-06-22 20:58:15 +0900

    feat :: add user swagger desc

--- src/main/java/com/appjam/dopamine/domain/user/presentation/UserController.java
+++ src/main/java/com/appjam/dopamine/domain/user/presentation/UserController.java
@@ -7,6 +7,8 @@ import com.appjam.dopamine.domain.user.response.ApiResponse;
 import com.appjam.dopamine.domain.user.response.JwtResponse;
 import com.appjam.dopamine.domain.user.service.UserServiceImpl;
 import com.appjam.dopamine.global.exception.BusinessException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Tag(name = "유저", description = "유저 관련 api 입니다.")
 public class UserController {
     private final UserService userService;
 
     private final UserServiceImpl userServiceImpl;
 
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
     @PostMapping("/signin")
 
     public ResponseEntity<?> authenticateUser(@RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok().body(userServiceImpl.authenticateAndGenerateJWT(loginRequest.getEmail(), loginRequest.getPassword()));
     }
 
+    @Operation(summary = "회원가입", description = "회원가입을 진행합니다.")
     @PostMapping("/signup")
     public ResponseEntity<?> registerAndAuthenticateUser(@RequestBody SignupRequest signupRequest) throws BusinessException {
 
